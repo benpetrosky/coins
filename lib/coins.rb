fabcb21c9f67668a1020a3a13aec17def0dc7b66
Ben Petrosky and Jesse Hoare Evans <jesse@>
2017-04-20 13:53:43 -0700

    adds experimental but awesome logic

--- lib/coins.rb
+++ lib/coins.rb
@@ -1,27 +1,39 @@
 class Fixnum
   define_method(:coin) do
-    input = self
-    counter = input
-    coin_counter = 0
-    penny = 1
-    nickel = 5
-    dime = 10
-    quarter = 25
-    cents_array= []
-    until counter.==(0)
-      cents_array.push("C")
-      counter = counter.-(1)
 
+    input = 66
+    second_input = input % 25
+    third_input = second_input % 10
+    fourth_input = third_input % 5
+
+    change = {25 => "quarter", 10 => "dime", 5 => "nickel", 1 => "penny"}
+
+    if input % 25 != 0 && input >= 25
+      until input <= 25
+      input = input.-(25)
+      change.fetch(25)
+      puts change.fetch(25)
+      end
     end
-    # if (input == 1)
-    #   ("one penny")
-    # elsif (input == 5)
-    #   ('one nickel')
-    # elsif (input == 10)
-    #   ('one dime')
-    # elsif (input == 25)
-    #  ("one quarter")
-    # end
-    cents_array
-  end
-end
+
+    if second_input % 10 != 0 && second_input >= 10
+      until second_input <= 10
+      second_input = second_input.-(10)
+      change.fetch(10)
+      puts change.fetch(10)
+      end
+    end
+
+
+    if third_input % 5 != 0 && third_input >= 5
+      until third_input <= 5
+      third_input = third_input.-(5)
+      change.fetch(5)
+      puts change.fetch(5)
+      end
+    end
+    until fourth_input % 5 == 0
+      fourth_input = fourth_input.-(1)
+      change.fetch(1)
+      puts change.fetch(1)
+    end
